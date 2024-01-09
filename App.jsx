b64884b171c7d817ad6c92efca27fb8ebe131059
Lucas Dias <lucas.allx@hotmail.com>
2024-01-09 18:26:38 -0300

    initial project

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <Text>Open up App.js to start working on your app!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
